ff2aa30517f6ecaa8981547806c8aa8948633d71
Gustavo <gustavofloripa98@gmail.com>
2022-04-08 22:56:21 -0300

    mexendo no login

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import styles from "../../styles/Home.module.scss";
 import Head from "next/head";
+import Image from "next/image";
 import Button from "../components/ui/Button";
 import Input from "../components/ui/Input";
 
 import { toast } from "react-toastify";
 
 export default function Home() {
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
@@ -21,39 +23,39 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Grow - Faça seu login</title>
-      </Head>
+       <Head>
+         <title>Grow - Faça seu login</title>
+       </Head>
 
-      <div>
-        <h1>Colocar a imagem</h1>
-      </div>
+        <div>
+            <h2>Image</h2>
+            <div className={styles.main}>
 
-      <main className={styles.main}>
-        <div>Grow</div>
+                <h2 className={styles.logo}>Grow</h2>
+                <h1>Login</h1>
+                <form onSubmit={handleLogin}>
+                    <label>Email:</label>
+                    <Input
+                        placeholder="Digite o seu e-mail"
+                        type="text"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <label>Senha:</label>
+                    <Input
+                        placeholder="Digite a sua senha"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Button loading={loading} type="submit">
+                        Entrar
+                    </Button>
 
-        <h2>Login</h2>
-
-        <form onSubmit={handleLogin}>
-          <label>Email:</label>
-          <Input
-            placeholder="Digite o seu e-mail"
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <label>Senha:</label>
-          <Input
-            placeholder="Digite a sua senha"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button loading={loading} type="submit">
-            Acessar
-          </Button>
-        </form>
-      </main>
+                    <a href={"www.google.com"} className={styles.link}>Não tem cadastro? Cadastre-se!</a>
+                </form>
+            </div>
+        </div>
     </div>
   );
 }
