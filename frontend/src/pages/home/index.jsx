0c3f89584ed01afc08432dcd152869adad58cb2a
Henrique <hitosanches@hotmail.com>
2022-04-13 20:45:33 -0600

    pagina home quase pronta

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { toast } from "react-toastify";
 import { AuthContext } from "../../contexts/AuthContext";
 import { Drower } from "../../components/ui/Drower";
+import { LogoHome } from "../../components/Logo";
 
 export default function Home() {
 
@@ -21,23 +22,28 @@ export default function Home() {
 
 
             <div className={styles.containerHistorico}>
-                <div className={styles.historicoUm} >
+                <button className={styles.historicoUm} >
                     <h1>#01</h1>
                     <h2>Aluguel 10293</h2>
                     <h2>10/04/2022</h2>
-                </div>
+                </button>
                 <div className={styles.historicoDois}>
-                    <h1>02</h1>
+                    <h1>#02</h1>
                     <h2>Aluguel 24002</h2>
                     <h2>03/04/2022</h2>
                 </div>
                 <div className={styles.historicoDois}>
-                    <h1>03</h1>
-                    <h2>aluguel 11583</h2>
+                    <h1>#03</h1>
+                    <h2>Aluguel 11583</h2>
                     <h2>27/03/2022</h2>
                 </div>
             </div>
-            <h1>Home</h1>
+            <div className={styles.grow}>
+                <LogoHome className={styles.filter} />
+            </div>
+            <footer className={styles.footer}>
+                <h1>Maria Roman Generative</h1>
+            </footer>
         </section>
     )
 }
