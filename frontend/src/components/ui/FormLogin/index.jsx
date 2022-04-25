9412d6468629af2a8453678dbc27fbbf510c6c4c
Gustavo <gustavofloripa98@gmail.com>
2022-04-25 10:53:10 -0300

    alterações nos forms

---
+++ frontend/src/components/ui/FormLogin/index.jsx
@@ -0,0 +1,64 @@
+import { useState, useContext } from "react";
+import styles from "./styles.module.scss";
+import Button from "../Button";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { AuthContext } from "../../../contexts/AuthContext";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    email: yup
+      .string()
+      .email("Precisa ser um e-mail válido!")
+      .required("Campo obrigatório!"),
+    password: yup
+        .string()
+        .min(3, "A senha precisa ter no mínimo 3 caracteres")
+        .required("Campo obrigatório!"),
+  })
+  .required();
+
+export function FormLogin() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const { signIn } = useContext(AuthContext);
+  const [loading, setLoading] = useState(false);
+
+  async function handleLogin(data) {
+    setLoading(true);
+    await signIn(data);
+    setLoading(false);
+  }
+  return (
+    <form onSubmit={handleSubmit(handleLogin)} className={styles.form}>
+      <label className={styles.textLogin}>Email:</label>
+      <input
+        {...register("email")}
+        className={errors.email?.type === 'required' || errors.email?.type === 'email'  ? styles.inputError : styles.input}
+        type="text"
+        placeholder="Digite o seu e-mail"
+      />
+      <p className={styles.errors}>{errors.email?.message}</p>
+
+      <label className={styles.textLogin}>Senha:</label>
+      <input
+        {...register("password")}
+        className={errors.email?.password === 'required' || errors.password?.type === 'min'  ? styles.inputError : styles.input}
+        type="password"
+        placeholder="Digite a sua senha"
+      />
+      <p className={styles.errors}>{errors.password?.message}</p>
+
+      <Button className={styles.button} loading={loading} type="submit">
+        ENTRAR
+      </Button>
+    </form>
+  );
+}
--- frontend/src/pages/signup/index.jsx
+++ frontend/src/pages/signup/index.jsx
@@ -1,41 +1,12 @@
-import {useState, useContext} from "react";
 import styles from "./styles.module.scss";
 import Head from "next/head";
 import Link from "next/link";
-import {LogoMenor} from "../../components/Logo"
-import Button from "../../components/ui/Button";
-import Input from "../../components/ui/Input";
-import {toast} from "react-toastify";
-import {AuthContext} from "../../contexts/AuthContext";
+import { LogoMenor } from "../../components/Logo"
+import { FormSignup } from "../../components/ui/FormSignup";
+
 
 export default function SignUp() {
 
-    const {signUp} = useContext(AuthContext);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [name, setName] = useState("");
-    const [registration, setRegistration] = useState("");
-    const [loading, setLoading] = useState(false);
-
-
-    async function handleSignup(event) {
-        event.preventDefault();
-
-        if (email === '' || password === '' || name === '' || registration === '') {
-            return toast.warning("Preencha todos os campos!");
-        }
-        setLoading(true);
-
-        let data = {
-            name,
-            email,
-            password,
-            registration
-        };
-        await signUp(data);
-        
-        setLoading(false);
-    }
 
     return (
         <div className={styles.container}>
@@ -45,51 +16,17 @@ export default function SignUp() {
 
             <div className={styles.modalContainerMain}>
 
-
                 <main className={styles.main}>
-
 
                     <LogoMenor/>
 
                     <h1>Cadastro</h1>
 
-                    <form onSubmit={handleSignup}>
-                        <label className={styles.textLogin}>Email:</label>
-                        <Input
-                            placeholder="Digite o seu e-mail"
-                            type="text"
-                            value={email}
-                            onChange={e => setEmail(e.target.value)}
-                        />
-                        <label className={styles.textLogin}>Nome:</label>
-                        <Input
-                            placeholder="Digite o seu nome"
-                            type="text"
-                            value={name}
-                            onChange={e => setName(e.target.value)}
-                        />
-                        <label className={styles.textLogin}>Confirme a senha:</label>
-                        <Input
-                            placeholder="Digite a sua senha"
-                            type="password"
-                            value={password}
-                            onChange={e => setPassword(e.target.value)}
-                        />
-                        <label className={styles.textLogin}>Matrícula:</label>
-                        <Input
-                            placeholder="Digite sua matrícula"
-                            type="text"
-                            value={registration}
-                            onChange={e => setRegistration(e.target.value)}
-                        />
-                        <Button type="submit" loading={loading}>
-                            CADASTRAR
-                        </Button>
+                    <FormSignup/>
 
-                        <Link href={"/"}>
-                            <a className={styles.link}>Faça o Login!</a>
-                        </Link>
-                    </form>
+                    <Link href={"/"}>
+                        <a className={styles.link}>Faça o Login!</a>
+                    </Link>
                 </main>
             </div>
         </div>
