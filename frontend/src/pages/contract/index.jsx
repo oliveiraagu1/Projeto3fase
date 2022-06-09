033da556ecdb8c1432739028433d3fb175f0371d
Gustavo <gustavofloripa98@gmail.com>
2022-06-08 21:41:48 -0300

    subindo o slidebar no contract

--- frontend/src/pages/contract/index.jsx
+++ frontend/src/pages/contract/index.jsx
@@ -1,33 +1,41 @@
-import Head from 'next/head';
-import styles from './styles.module.scss';
-import { LogoMenor } from '../../components/Logo';
-import ContractItem from '../../components/ui/ContractItem'
+import { useState, useContext } from "react";
+import Head from "next/head";
+import styles from "./styles.module.scss";
+import ContractItem from "../../components/ui/ContractItem";
+import SlideBar from "../../components/ui/SlideBar";
+import { LogoMenor } from "../../components/Logo";
 
-export default function Contract(){
+export default function Contract() {
+  const [visible, setVisible] = useState(false);
 
-    return(
+  return (
+    <div className={ visible ? styles.container : styles.containerClose}>
+      <Head>
+        <title>Grow - Tela contrato</title>
+      </Head>
 
-        <div className={styles.container}>
-            <Head>
-                <title>Grow - Tela contrato</title>
-            </Head>
-                
-                <div className={styles.image}>
-                    <LogoMenor/>
-                </div>
-                <div>
-                    <h1>Meu histórico</h1>
-                </div>
+      <button
+        className={styles.burguerContainer}
+        onClick={() => setVisible(!visible)}
+      >
+        <SlideBar visible={visible} />
+      </button>
 
-                <div className={styles.grid}>
-                    <ContractItem data={{id: 1}}/>
-                    <ContractItem data={{id: 2}}/>
-                    <ContractItem  data={{id: 3}}/>
-                    <ContractItem  data={{id: 4}}/>
-                    <ContractItem  data={{id: 5}}/>
-                    <ContractItem  data={{id: 6}}/>
-                    
-                </div>
-        </div>
-    )
+      <div className={styles.image}>
+        <LogoMenor />
+      </div>
+      <div>
+        <h1>Meu histórico</h1>
+      </div>
+
+      <div className={styles.grid}>
+        <ContractItem data={{ id: 1 }} />
+        <ContractItem data={{ id: 2 }} />
+        <ContractItem data={{ id: 3 }} />
+        <ContractItem data={{ id: 4 }} />
+        <ContractItem data={{ id: 5 }} />
+        <ContractItem data={{ id: 6 }} />
+      </div>
+    </div>
+  );
 }
