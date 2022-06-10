1f84e2f3250ceca9a7b44530f7b69527bb679ca8
Gustavo <gustavofloripa98@gmail.com>
2022-06-10 20:52:43 -0300

    historico refatorado e criando tela contrato

--- frontend/src/pages/contract/index.jsx
+++ frontend/src/pages/contract/index.jsx
@@ -1,10 +1,43 @@
-import styles from './styles.module.scss';
+import React, { useState, useContext } from "react";
+import Head from "next/head";
+import styles from "./styles.module.scss";
+import SlideBar from "../../components/ui/SlideBar";
+import { FormContract } from "../../components/ui/FormContract";
+import { LogoMenor } from "../../components/Logo";
 
-export default function Contract(){
+export default function Contract() {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div className={visible ? styles.container : styles.containerClose}>
+      <Head>
+        <title>Grow - Cadastre um contrato</title>
+      </Head>
+
+      <button
+        className={styles.burguerContainer}
+        onClick={() => setVisible(!visible)}
+      >
+        <SlideBar visible={visible} />
+      </button>
+
+      <div className={styles.image}>
+        <LogoMenor />
+      </div>
     
-    return(
-        <div>
-            ....
+      <main className={styles.main}>
+        <div className={styles.title}>
+          <h1>Registe um contrato</h1>
         </div>
-    )
+
+        <div>
+            <FormContract/>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <p>Testando</p>
+      </footer>
+    </div>
+  );
 }
